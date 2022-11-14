01e2858c12d5d0c57a37ff0bb72141c47dfdf52b
xheize <yejun1101@naver.com>
2022-11-14 23:26:49 +0900

    backend 뼈대 제작

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+const port = process.env.port
 
-app.listen(4000) => {
-    console.log("listen server 4000")
-}
+app.listen( port, () => {
+    console.log(`This Server Running 5000 Port Number`)
+})
 
 
-app.post()
+app.get('/', (res, req) => {
+    res.status(200).json({"heartBeat": True})
+})
+
+app.post('/', (res, req) => {
+    res.status(200).json({"heartBeat": True})   
+})
